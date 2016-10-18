8598f35cebfed227fc433cbbd87ef3021c01935b
Matus Goljer <goljer@logio.cz>
2016-10-18 16:26:41 +0200

    Load the gnuplot script from project directory

--- swb-graph.el
+++ swb-graph.el
@@ -1,6 +1,9 @@
+(defvar swb-package-directory (file-name-directory load-file-name))
+
 (defun swb-gnuplot (result outfile)
   (with-temp-buffer
+    (cd swb-package-directory)
     (--each result
       (insert (s-join " " it) "\n"))
     (call-process-region (point-min) (point-max) "gnuplot" nil nil nil
-                         "-e" (format "outfile='%s'" outfile) "/home/matus/test/plot.gnu")))
+                         "-e" (format "outfile='%s'" outfile) "plot.gnu")))
