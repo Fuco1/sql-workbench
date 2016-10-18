c0882849cea41e780f45de9c62217abc74a47600
Matus Goljer <matus.goljer@gmail.com>
2016-10-18 23:21:32 +0200

    Render gnuplot from temporary file

--- swb-graph.el
+++ swb-graph.el
@@ -1,9 +1,14 @@
 (defvar swb-package-directory (file-name-directory load-file-name))
 
 (defun swb-gnuplot (result outfile)
-  (with-temp-buffer
-    (cd swb-package-directory)
-    (--each result
-      (insert (s-join " " it) "\n"))
-    (call-process-region (point-min) (point-max) "gnuplot" nil nil nil
-                         "-e" (format "outfile='%s'" outfile) "plot.gnu")))
+  (let ((infile (make-temp-file "swb-gnuplot-source")))
+    (with-temp-file infile
+      (cd swb-package-directory)
+      (--each result
+        (insert (s-join " " it) "\n")))
+    (call-process "gnuplot" nil nil nil
+                  "-e" (format "outfile='%s'" outfile)
+                  "-e" (format "infile='%s'" infile)
+                  "-e" (format "termheight='%s'" (* 25 (default-font-height)))
+                  "plot.gnu")
+    (delete-file infile)))
