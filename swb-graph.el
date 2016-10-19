534fef95c0c478dd1bb65e7f9543619d0fe5f991
Matus Goljer <goljer@logio.cz>
2016-10-19 14:56:39 +0200

    Full path to plot script

--- swb-graph.el
+++ swb-graph.el
@@ -3,12 +3,11 @@
 (defun swb-gnuplot (result outfile)
   (let ((infile (make-temp-file "swb-gnuplot-source")))
     (with-temp-file infile
-      (cd swb-package-directory)
       (--each result
         (insert (s-join " " it) "\n")))
     (call-process "gnuplot" nil nil nil
                   "-e" (format "outfile='%s'" outfile)
                   "-e" (format "infile='%s'" infile)
                   "-e" (format "termheight='%s'" (* 25 (default-font-height)))
-                  "plot.gnu")
+                  (concat swb-package-directory "/plot.gnu"))
     (delete-file infile)))
