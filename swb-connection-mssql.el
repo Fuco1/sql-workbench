285dbab15b58042dbb9d822722dc9ccf412286cd
Matus Goljer <matus.goljer@gmail.com>
2021-05-18 21:03:56 +0200

    feat(mssql): optimize the result set type query

--- swb-connection-mssql.el
+++ swb-connection-mssql.el
@@ -82,6 +82,15 @@ BUFFER is the buffer with the raw query output"
 This is called on the raw query output after all the output was
 received."
   (with-current-buffer buffer
+    ;; this is the first "n rows affected" for inserting the
+    ;; sp_describe_first_result_set result into a temporary table
+    (delete-region
+     (point-min)
+     (save-excursion
+       (goto-char (point-min))
+       (re-search-forward "rows affected")
+       (forward-line 1)
+       (point)))
     (let ((data (delete-and-extract-region
                  (point-min)
                  (save-excursion
@@ -106,12 +115,17 @@ every time input is received until it is unset or set to nil."
   (comint-send-string
    (get-buffer-process (current-buffer))
    (format
-    "DECLARE @query nvarchar(max) = \"%s\";%s
+    "%sDECLARE @query nvarchar(max) = \"%s\";%s
 EXEC sp_executesql @query;
 go"
+    (if get-metadata
+        "create table #swb_query_meta (is_hidden bit NOT NULL, column_ordinal int NOT NULL, name sysname NULL, is_nullable bit NOT NULL, system_type_id int NOT NULL, system_type_name nvarchar(256) NULL, max_length smallint NOT NULL, precision tinyint NOT NULL, scale tinyint NOT NULL, collation_name sysname NULL, user_type_id int NULL, user_type_database sysname NULL, user_type_schema sysname NULL, user_type_name sysname NULL, assembly_qualified_type_name nvarchar(4000), xml_collection_id int NULL, xml_collection_database sysname NULL, xml_collection_schema sysname NULL, xml_collection_name sysname NULL, is_xml_document bit NOT NULL, is_case_sensitive bit NOT NULL, is_fixed_length_clr_type bit NOT NULL, source_server sysname NULL, source_database sysname NULL, source_schema sysname NULL, source_table sysname NULL, source_column sysname NULL, is_identity_column bit NULL, is_part_of_unique_key bit NULL, is_updateable bit NULL, is_computed_column bit NULL, is_sparse_column_set bit NULL, ordinal_in_order_by_list smallint NULL, order_by_list_length smallint NULL, order_by_is_descending smallint NULL, tds_type_id int NOT NULL, tds_length int NOT NULL, tds_collation_id int NULL, tds_collation_sort_id tinyint NULL);\n"
+      "")
     (replace-regexp-in-string "\"" "\"\"" query)
     (if get-metadata
-        "\nEXEC sp_describe_first_result_set @query, null, 0;"
+        "\ninsert #swb_query_meta EXEC sp_describe_first_result_set @query, null, 0;
+select name, system_type_name from #swb_query_meta;
+drop table #swb_query_meta"
       "")))
   (comint-send-input nil t))
 
